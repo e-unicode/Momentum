aebc78b76f8a08bd69d833eaeefe147aa5a73671
EUNI <104560558+rlool@users.noreply.github.com>
2022-12-05 15:22:19 +0900

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,23 @@
+const images = [
+    "선장_찐4.jpg",
+    "선장_찐5.jpg",
+    "선장_찐6.jpg",
+    "슈퍼참치_찐1.jpg",
+    "슈퍼참치_찐2.jpg",
+    "슈퍼참치_찐3.jpg",
+    "어부_진2.jpg",
+    "어부_진3.jpg",
+    "어부_진4.jpg",
+    "어부_진6.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)]
+
+
+const bgImage = document.createElement("img");
+bgImage.src = `./img/${chosenImage}`
+
+leftBg = document.querySelector(".left-container");
+leftBg.appendChild(bgImage);
+
+  
