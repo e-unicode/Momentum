89b55ae33ea12efd23408a48b18e3d1991d1e87f
EUNI <104560558+rlool@users.noreply.github.com>
2022-12-05 18:12:57 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,14 +1,14 @@
 const images = [
-    "선장_찐4.jpg",
-    "선장_찐5.jpg",
-    "선장_찐6.jpg",
-    "슈퍼참치_찐1.jpg",
-    "슈퍼참치_찐2.jpg",
-    "슈퍼참치_찐3.jpg",
-    "어부_진2.jpg",
-    "어부_진3.jpg",
-    "어부_진4.jpg",
-    "어부_진6.jpg",
+    "jin1.jpg",
+    "jin2.jpg",
+    "jin3.jpg",
+    "jin4.jpg",
+    "jin5.jpg",
+    "jin6.jpg",
+    "jin7.jpg",
+    "jin8.jpg",
+    "jin9.jpg",
+    "jin10.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
